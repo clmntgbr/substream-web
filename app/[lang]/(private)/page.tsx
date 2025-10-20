91c48e13ea24b1acfd5aeb9af4b72962b62272c6
Clément <33451311+clmntgbr@users.noreply.github.com>
2025-10-20 15:08:50 +0200

    Add YouTube URL Input Handling and Validation to HomePage
    - Introduced state management for YouTube URL input, including validation using `YoutubeUrlSchema`.
    - Implemented user feedback with toast notifications for invalid URLs.
    - Enhanced the upload button functionality to process and preview valid YouTube URLs.
    - Cleaned up import statements for better organization and maintainability.

--- app/[lang]/(private)/page.tsx
+++ app/[lang]/(private)/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { getColumns } from "@/components/home/queue/Columns";
 import { Logo } from "@/components/Logo";
+import { YoutubeUrlSchema } from "@/components/misc/YoutubeUrlSchema";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { InputGroup, InputGroupAddon, InputGroupInput } from "@/components/ui/input-group";
@@ -10,10 +11,17 @@ import { useTranslations } from "@/lib/use-translations";
 import { ColumnFiltersState, PaginationState, SortingState } from "@tanstack/react-table";
 import { Link2Icon, PaperclipIcon } from "lucide-react";
 import { useParams } from "next/navigation";
-import { useCallback, useEffect, useMemo, useRef } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { toast } from "sonner";
 
 const HomePage = () => {
   const { user } = useAuth();
+  const [url, setUrl] = useState<string>("");
+  const [urlInput, setUrlInput] = useState("");
+  const [isPreviewOpen, setIsPreviewOpen] = useState(false);
+  const [isProcessing, setIsProcessing] = useState(false);
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+
   const { state, getStreams, pageCount } = useStreams();
   const { lang } = useParams();
   const t = useTranslations();
@@ -104,12 +112,33 @@ const HomePage = () => {
               {t.home.upload.file}
             </Button>
             <InputGroup className="flex rounded-4xl pr-0 w-full shadow-none">
-              <InputGroupInput className="rounded-r-none " placeholder="https://youtu.be/uC0RFwhrWLE?si=tG6hdG1LIgsTgnun" />
+              <InputGroupInput
+                value={urlInput}
+                onChange={(e) => {
+                  setUrlInput(e.target.value);
+                }}
+                className="rounded-r-none "
+                placeholder="https://youtu.be/uC0RFwhrWLE?si=tG6hdG1LIgsTgnun"
+              />
               <InputGroupAddon align="inline-end" className="p-0">
                 <Button
                   variant="outline"
                   className="border-l-1 border-t-0 border-b-0 border-r-0 mr-2 shadow-none bg-transparent hover:bg-muted/30 hover:text-accent-foreground dark:text-white cursor-pointer rounded-4xl text-black rounded-l-4xl"
-                  onClick={() => {}}
+                  onClick={() => {
+                    setIsProcessing(true);
+                    try {
+                      YoutubeUrlSchema.parse(urlInput);
+                      setUrl(urlInput);
+                      setIsPreviewOpen(true);
+                      setUrlInput("");
+                    } catch {
+                      toast.error("Invalid YouTube URL", {
+                        description: "Please enter a valid YouTube URL",
+                      });
+                    } finally {
+                      setIsProcessing(false);
+                    }
+                  }}
                 >
                   <Link2Icon className="size-4" />
                   {t.home.upload.url}
